9ef54cfdd3d740444313d326a6b687f383dff902
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-12-28 16:48:50 -0500

    add tests to runner

--- test/Main.purs
+++ test/Main.purs
@@ -5,9 +5,11 @@ import Prelude
 import Effect (Effect)
 import Test.MCache.Laws as MCacheLaws
 import Test.Unit.Main (runTest)
+import Test.Zipper.Unit as ZipperUnit
 import Test.ZipperM.Unit as ZipperMUnit
 
 main :: Effect Unit
 main = do
     runTest ZipperMUnit.tests
+    runTest ZipperUnit.tests
     runTest MCacheLaws.tests
