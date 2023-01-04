8c8a7dfa3b10c2662121c722f112a1e121db233f
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2023-01-04 13:48:35 -0500

    walk necklaces

--- test/Utils.purs
+++ test/Utils.purs
@@ -7,6 +7,8 @@ import Data.BufferedZipper (BufferedZipper)
 import Data.BufferedZipper as BZ
 import Data.MCache (MCache(..), force)
 import Data.Maybe (Maybe(..))
+import Data.Necklace (Necklace)
+import Data.Necklace as Necklace
 import Data.ZipperM (ZipperM)
 import Data.ZipperM as ZipperM
 import Test.QuickCheck (class Arbitrary, arbitrary)
@@ -14,7 +16,7 @@ import Test.QuickCheck (class Arbitrary, arbitrary)
 
 data PN = P | N
 
-type ZipperPath = Array PN
+type WalkPath = Array PN
 
 newtype MCacheM m a = MCacheM (m (MCache m a))
 
@@ -25,10 +27,13 @@ instance arbitraryMCacheM :: (Applicative m, Arbitrary (m a)) => Arbitrary (MCac
         let cache = MCache m Nothing
         pure <<< MCacheM $ if b then pure cache else force cache
 
-walkZipperM :: forall m a. Monad m => ZipperPath -> ZipperM m a -> m (Array a)
+walkNecklace :: forall a. WalkPath -> Necklace a -> Array a
+walkNecklace = walk (Just <<< Necklace.next) Necklace.focus (Just <<< Necklace.next)
+
+walkZipperM :: forall m a. Monad m => WalkPath -> ZipperM m a -> m (Array a)
 walkZipperM = walkM ZipperM.prev ZipperM.focus ZipperM.next
 
-walkBufferedZipper :: forall m a. Monad m => ZipperPath -> BufferedZipper m a -> m (Array a)
+walkBufferedZipper :: forall m a. Monad m => WalkPath -> BufferedZipper m a -> m (Array a)
 walkBufferedZipper = walkM BZ.prev BZ.focus BZ.next
 
 walk ::
@@ -36,7 +41,7 @@ walk ::
     . (t a -> Maybe (t a))
     -> (t a -> a)
     -> (t a -> Maybe (t a))
-    -> ZipperPath
+    -> WalkPath
     -> t a
     -> Array a
 walk prev focus next path xs = case uncons path of
@@ -55,7 +60,7 @@ walkM ::
     => (t m a -> m (Maybe (t m a)))
     -> (t m a -> a)
     -> (t m a -> m (Maybe (t m a)))
-    -> ZipperPath
+    -> WalkPath
     -> t m a
     -> m (Array a)
 walkM prev focus next path xs = case uncons path of
