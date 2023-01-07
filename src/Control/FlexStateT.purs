8392260856be45f3b90251634e64e830639ef735
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2023-01-07 16:55:47 -0500

    monad time

--- src/Control/FlexStateT.purs
+++ src/Control/FlexStateT.purs
@@ -3,6 +3,7 @@ module FlexStateT where
 import Prelude
 
 import Data.Tuple (Tuple(..), fst, snd)
+import Utils (mapSnd)
 
 
 data FlexStateT s s' m a = FlexStateT (s -> m (Tuple a s'))
@@ -20,14 +21,15 @@ instance functorFlexStateT :: Functor m => Functor (FlexStateT s s' m) where
     map f (FlexStateT statef) = 
         FlexStateT (\s -> map (\(Tuple b s') -> Tuple (f b) s') (statef s))
 
-instance applyFlexStateT :: Monad m => Apply (FlexStateT s s m) where
+instance applyFlexStateT :: (Monoid s', Monad m) => Apply (FlexStateT s s' m) where
     apply = ap
 
-instance applicativeFlexStateT :: Monad m => Applicative (FlexStateT s s m) where
-    pure x = FlexStateT (\s -> pure $ Tuple x s)
+instance applicativeFlexStateT :: (Monoid s', Monad m) => Applicative (FlexStateT s s' m) where
+    pure x = FlexStateT (\_ -> pure $ Tuple x mempty)
 
-instance bindFlexStateT :: Monad m => Bind (FlexStateT s s m) where
+instance bindFlexStateT :: (Monoid s', Monad m) => Bind (FlexStateT s s' m) where
     bind (FlexStateT fstate) f = FlexStateT \s ->
-        fstate s >>= \(Tuple v s') -> case f v of FlexStateT st -> st s'
+        fstate s >>= \(Tuple v s') -> case f v of
+            FlexStateT st -> map (mapSnd $ append s') (st s)
 
-instance monadFlexStateT :: Monad m => Monad (FlexStateT s s m)
+instance monadFlexStateT :: (Monoid s', Monad m) => Monad (FlexStateT s s' m)
--- src/Utils.purs
+++ src/Utils.purs
@@ -6,6 +6,7 @@ import Data.Array as Array
 import Data.List.Lazy (List, nil)
 import Data.List.Lazy as List
 import Data.Maybe (fromMaybe)
+import Data.Tuple (Tuple(..))
 
 
 tail' :: forall a. List a -> List a
@@ -16,3 +17,6 @@ arrayTail' = fromMaybe [] <<< Array.tail
 
 init' :: forall a. List a -> List a
 init' = fromMaybe nil <<< List.init
+
+mapSnd :: forall a b c. (b -> c) -> Tuple a b -> Tuple a c
+mapSnd f (Tuple x y) = Tuple x (f y)
