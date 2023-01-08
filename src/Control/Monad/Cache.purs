7d0c3a1b0c3a5cacc88f1c64e2e76003a7a88530
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2023-01-08 17:11:35 -0500

    rename CacheT

--- src/Control/Monad/Cache.purs
+++ src/Control/Monad/Cache.purs
@@ -11,46 +11,46 @@ import Data.Tuple (Tuple(..))
 
 
 -- TODO Add cache size so its not always infinite
--- TODO rename CacheT and make Cache type with Identity
-newtype Cache m k v a = Cache (StateT (HashMap k v) m a)
+-- TODO make Cache type with Identity
+newtype CacheT m k v a = CacheT (StateT (HashMap k v) m a)
 
-instance semigroupCache :: (Hashable k, Monad m, Semigroup a) => Semigroup (Cache m k v a) where
-    append (Cache (StateT fx)) (Cache (StateT fy)) = Cache (StateT \s -> do
+instance semigroupCacheT :: (Hashable k, Monad m, Semigroup a) => Semigroup (CacheT m k v a) where
+    append (CacheT (StateT fx)) (CacheT (StateT fy)) = CacheT (StateT \s -> do
         Tuple x s' <- fx s
         Tuple x' s'' <- fy s'
         -- union keeps the value from the left in the event of duplicates
         pure $ Tuple (x <> x') (M.union s'' s'))
 
-instance functorCache :: (Applicative m, Hashable k) => Functor (Cache m k v) where
-    map f (Cache x) = Cache (map f x)
+instance functorCacheT :: (Applicative m, Hashable k) => Functor (CacheT m k v) where
+    map f (CacheT x) = CacheT (map f x)
 
-instance applyCache :: (Monad m, Hashable k) => Apply (Cache m k v) where
+instance applyCacheT :: (Monad m, Hashable k) => Apply (CacheT m k v) where
     apply = ap
 
-instance applicativeCache :: (Monad m, Hashable k) => Applicative (Cache m k v) where
-    pure x = Cache (StateT \s -> pure $ Tuple x s)
+instance applicativeCacheT :: (Monad m, Hashable k) => Applicative (CacheT m k v) where
+    pure x = CacheT (StateT \s -> pure $ Tuple x s)
 
-instance bindCache :: (Monad m, Hashable k) => Bind (Cache m k v) where
-    bind :: forall a b. Cache m k v a -> (a -> Cache m k v b) -> Cache m k v b
-    bind (Cache (StateT fs)) f = Cache (StateT \s -> do
+instance bindCacheT :: (Monad m, Hashable k) => Bind (CacheT m k v) where
+    bind :: forall a b. CacheT m k v a -> (a -> CacheT m k v b) -> CacheT m k v b
+    bind (CacheT (StateT fs)) f = CacheT (StateT \s -> do
         Tuple x m <- fs s
-        let (Cache (StateT fs')) = (f x)
+        let (CacheT (StateT fs')) = (f x)
         Tuple y m' <- fs' s
         pure $ Tuple y (M.union m' m))
 
-instance monadCache :: (Monad m, Hashable k) => Monad (Cache m k v)
+instance monadCacheT :: (Monad m, Hashable k) => Monad (CacheT m k v)
 
-instance monadCacheCache :: (Hashable k, Monad m) => MonadCache (Cache m k v) k v where
-    cache f = Cache (StateT $ pure <<< f)
+instance monadCacheCacheT :: (Hashable k, Monad m) => MonadCache (CacheT m k v) k v where
+    cache f = CacheT (StateT $ pure <<< f)
 
-empty :: forall m k v. Applicative m => Cache m k v Unit
-empty = Cache (StateT \_ -> pure $ Tuple unit M.empty)
+empty :: forall m k v. Applicative m => CacheT m k v Unit
+empty = CacheT (StateT \_ -> pure $ Tuple unit M.empty)
 
-runCache :: forall m k v a. Cache m k v a → (HashMap k v) → m (Tuple a (HashMap k v))
-runCache (Cache x) = runStateT x
+runCacheT :: forall m k v a. CacheT m k v a → (HashMap k v) → m (Tuple a (HashMap k v))
+runCacheT (CacheT x) = runStateT x
 
-evalCache :: forall m k v a. Functor m ⇒ Cache m k v a → (HashMap k v) → m a
-evalCache (Cache x) = evalStateT x
+evalCacheT :: forall m k v a. Functor m ⇒ CacheT m k v a → (HashMap k v) → m a
+evalCacheT (CacheT x) = evalStateT x
 
-execCache :: forall m k v a. Functor m ⇒ Cache m k v a → (HashMap k v) → m (HashMap k v)
-execCache (Cache x) = execStateT x
+execCacheT :: forall m k v a. Functor m ⇒ CacheT m k v a → (HashMap k v) → m (HashMap k v)
+execCacheT (CacheT x) = execStateT x
